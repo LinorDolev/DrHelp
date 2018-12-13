52c49f1e355c33accdaf7ad80cf0409a185d1026
shiranSofer <shiran.soferg@gmail.com>
2018-12-13 20:16:27 +0200

    Firebase SDK and Google Services

--- app/src/main/java/com/example/shiran/drhelp/LoginActivity.java
+++ app/src/main/java/com/example/shiran/drhelp/LoginActivity.java
@@ -1,14 +1,29 @@
 package com.example.shiran.drhelp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView toRegister_TextView;
+    Intent toRegister_Intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        //try
+
+        toRegister_TextView = findViewById(R.id.link_to_register);
+        toRegister_TextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               toRegister_Intent = new Intent(getApplicationContext(), RegisterActivity.class);
+               startActivity(toRegister_Intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/shiran/drhelp/RegisterActivity.java
+++ app/src/main/java/com/example/shiran/drhelp/RegisterActivity.java
@@ -1,13 +1,57 @@
 package com.example.shiran.drhelp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText dateOfBirth_EditText;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        dateOfBirth_EditText = findViewById(R.id.dateOfBirth_editText_register);
+        dateOfBirth_EditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment dialogFragment = new DatePickerDialogTheme();
+                dialogFragment.show(getSupportFragmentManager(), "Theme");
+            }
+        });
+    }
+
+
+    public static class DatePickerDialogTheme extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+        EditText dateOfBirth_EditText;
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            final Calendar calendar = Calendar.getInstance();
+            int year = calendar.get(Calendar.YEAR);
+            int month = calendar.get(Calendar.MONTH);
+            int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), this, year, month, day);
+            return datePickerDialog;
+        }
+
+        @Override
+        public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+            dateOfBirth_EditText = getActivity().findViewById(R.id.dateOfBirth_editText_register);
+            dateOfBirth_EditText.setText(day + "/" + (month + 1) + "/" + year);
+        }
     }
 }
