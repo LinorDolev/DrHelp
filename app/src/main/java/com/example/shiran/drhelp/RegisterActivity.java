1cd099abe25c0759859e498159f25a2edcab8fd4
shiranSofer <shiran.soferg@gmail.com>
2018-12-15 23:41:15 +0200

    Registration and Authentication works

--- app/src/main/java/com/example/shiran/drhelp/RegisterActivity.java
+++ app/src/main/java/com/example/shiran/drhelp/RegisterActivity.java
@@ -1,20 +1,36 @@
 package com.example.shiran.drhelp;
 
-import android.app.DatePickerDialog;
-import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
-import android.widget.DatePicker;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import java.util.Calendar;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    EditText dateOfBirth_EditText;
+    private EditText editText_userFirstName;
+    private EditText editText_userLastName;
+    private EditText editText_userEmail;
+    private EditText editText_userPassword;
+    private Button button_Register;
 
+    //--datebase--//
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+    private FirebaseAuth firebaseAuth;
 
 
     @Override
@@ -22,36 +38,89 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        dateOfBirth_EditText = findViewById(R.id.dateOfBirth_editText_register);
-        dateOfBirth_EditText.setOnClickListener(new View.OnClickListener() {
+        initRegistrationReferences();
+        initDatabaseComponents();
+        registerUser();
+
+    }
+
+    private void initRegistrationReferences(){
+        editText_userFirstName = findViewById(R.id.firstName_editText_register);
+        editText_userLastName = findViewById(R.id.lastName_editText_register);
+        editText_userEmail = findViewById(R.id.email_editText_register);
+        editText_userPassword = findViewById(R.id.password_editText_register);
+        button_Register = findViewById(R.id.btn_register);
+    }
+
+    private void initDatabaseComponents() {
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("users");
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    private void registerUser(){
+        button_Register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                DialogFragment dialogFragment = new DatePickerDialogTheme();
-                dialogFragment.show(getSupportFragmentManager(), "Theme");
+                String firstName = editText_userFirstName.getText().toString().trim();
+                String lastName = editText_userLastName.getText().toString().trim();
+                String email = editText_userEmail.getText().toString().trim();
+                String password = editText_userPassword.getText().toString().trim();
+
+                if(!isValidForm(firstName, lastName, email, password)){
+                    Log.d("register-user:", "invalid form.");
+                    return;
+                }
+
+                Log.d("register-user:", "valid form.");
+                createUser(firstName, lastName, email, password);
             }
         });
     }
 
+    private boolean isValidForm(String firstName, String lastName, String email, String password) {
 
-    public static class DatePickerDialogTheme extends DialogFragment implements DatePickerDialog.OnDateSetListener {
-
-        EditText dateOfBirth_EditText;
-
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            final Calendar calendar = Calendar.getInstance();
-            int year = calendar.get(Calendar.YEAR);
-            int month = calendar.get(Calendar.MONTH);
-            int day = calendar.get(Calendar.DAY_OF_MONTH);
-
-            DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), this, year, month, day);
-            return datePickerDialog;
+        if(TextUtils.isEmpty(firstName)){
+            editText_userFirstName.setError("First Name Required.");
+            return false;
+        }
+        if(TextUtils.isEmpty(lastName)){
+            editText_userLastName.setError("Last Name Required.");
+            return false;
+        }
+        if(TextUtils.isEmpty(email)){
+            editText_userEmail.setError("Email Required.");
+            return false;
+        }
+        if(TextUtils.isEmpty(password)){
+            editText_userPassword.setError("Password Required.");
+            return false;
         }
 
-        @Override
-        public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-            dateOfBirth_EditText = getActivity().findViewById(R.id.dateOfBirth_editText_register);
-            dateOfBirth_EditText.setText(day + "/" + (month + 1) + "/" + year);
-        }
+        return true;
+    }
+
+    private void createUser(final String firstName, final String lastName, final String email, final String password) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(
+                        this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(!task.isSuccessful()){
+                    Toast.makeText(RegisterActivity.this, "Authentication failed."
+                            + task.getException(), Toast.LENGTH_SHORT).show();
+                    Log.d("create-user:", "Authentication failed.");
+                } else {
+                    String userId  = firebaseAuth.getUid();
+                    UserEntity user = new UserEntity(userId, firstName, lastName, email, password);
+                    databaseReference.child(userId).setValue(user);
+                    Toast.makeText(RegisterActivity.this, "Registration succeeded."
+                            , Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                    finish();
+                    Log.d("create-user:", "create user succeeded.");
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/shiran/drhelp/UserEntity.java
@@ -0,0 +1,109 @@
+package com.example.shiran.drhelp;
+
+public class UserEntity {
+
+    private final Boolean AVAILABLE = true;
+    private final String LANGUAGE = "English";
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private Boolean available;
+    private String language;
+
+    public UserEntity() {
+    }
+
+    public UserEntity(String id, String firstName, String lastName,
+                      String email, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.available = AVAILABLE;
+        this.language = LANGUAGE;
+    }
+
+    public UserEntity(String id, String firstName, String lastName,
+                      String email, String password, Boolean available, String language) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.available = available;
+        this.language = language;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    @Override
+    public String toString() {
+        return "UserEntity{" +
+                "id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", available='" + available + '\'' +
+                ", language='" + language + '\'' +
+                '}';
+    }
+}
